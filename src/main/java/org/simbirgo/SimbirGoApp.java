35cf29e3533bac93f45dd946377196f0831b30b7
KataevEvgeniy <jackman.bup@gmail.com>
2023-10-09 14:32:25 +0500

    init

---
+++ src/main/java/org/simbirgo/SimbirGoApp.java
@@ -0,0 +1,17 @@
+package org.simbirgo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirGoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimbirGoApp.class,args);
+    }
+
+
+
+
+}
