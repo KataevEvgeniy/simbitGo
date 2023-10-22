fc9c82ce5a38c4519a9a3db1d56160bdbe1a8a6e
Kataev Evgeniy <jackman.bup@gmail.com>
2023-10-22 16:38:39 +0500

    add WebFilterWithout SpringSecurity

--- src/main/java/org/simbirgo/configs/JwtAuthenticationFilter.java
+++ src/main/java/org/simbirgo/configs/JwtAuthenticationFilter.java
@@ -4,16 +4,12 @@ import org.simbirgo.entities.UserEntity;
 import org.simbirgo.repositories.UserEntityRepository;
 import org.simbirgo.services.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -26,14 +22,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 
     private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
+
 
     private final UserEntityRepository userRepository;
 
     @Autowired
-    JwtAuthenticationFilter(JwtService jwtService,UserDetailsService userDetailsService,UserEntityRepository userRepository){
+    JwtAuthenticationFilter(JwtService jwtService,UserEntityRepository userRepository){
             this.jwtService = jwtService;
-            this.userDetailsService = userDetailsService;
             this.userRepository = userRepository;
     }
 
